0ff2833c925023eb94ae36a21a39ff33e69acb65
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-04-08 11:05:08 +0600

    initial react router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-const App = () => {
-  return <h1>Welcome to HeroGadget</h1>
-}
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Outlet } from 'react-router-dom';
 
-export default App
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <div className='min-h-[calc(100vh-157px)]'>
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default App;
